b8a1fc8e4988bcb6c5e516b75b4e79c8031344f1
Higic <onni.alasaari@gmail.com>
2024-04-26 17:31:30 +0300

    updated final test

--- src/test/java/HinnoittelijaMockitoTest.java
+++ src/test/java/HinnoittelijaMockitoTest.java
@@ -38,6 +38,28 @@ public class HinnoittelijaMockitoTest {
         assertEquals(loppuSaldo, asiakas.getSaldo(), 0.001);
         verify(hinnoittelijaMock, times(2)).getAlennusProsentti(asiakas, tuote);
     }
+    @Test
+    public void testaaKasittelijaWithBigPrice() {
+        // Arrange
+        float alkuSaldo = 100.0f;
+        float listaHinta = 130.0f;
+        float alennus = 20.0f;
+        float loppuSaldo = alkuSaldo - (listaHinta * (1 - alennus / 100));
+
+        Asiakas asiakas = new Asiakas(alkuSaldo);
+        Tuote tuote = new Tuote("TDD in Action", listaHinta);
+        // Record
+            when(hinnoittelijaMock.getAlennusProsentti(asiakas, tuote))
+                .thenReturn(alennus);
+        // Act
+        TilaustenKäsittely kasittelija = new TilaustenKäsittely();
+        kasittelija.setHinnoittelija(hinnoittelijaMock);
+        kasittelija.käsittele(new Tilaus(asiakas, tuote));
+
+        // Assert
+        assertEquals(loppuSaldo, asiakas.getSaldo(), 0.001);
+        verify(hinnoittelijaMock, times(2)).getAlennusProsentti(asiakas, tuote);
+    }
 
 
 }
